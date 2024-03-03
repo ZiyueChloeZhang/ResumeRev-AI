d0b5b2b529c170a0616c4a42d2799d96a4d07947
Ziyue (Chloe) Zhang <41217816+ZiyueChloeZhang@users.noreply.github.com>
2024-03-02 17:53:06 -0800

    final (#2)
    * test s3
    
    * added upload jd/resume
    
    * test embedding
    
    * result
    
    * fix ui

--- app/main.py
+++ app/main.py
@@ -1,84 +1,140 @@
+import os
+
 import streamlit as st
 import boto3
-from botocore.exceptions import NoCredentialsError
-import uuid
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms.bedrock import Bedrock
+from langchain.embeddings import BedrockEmbeddings
+from langchain.vectorstores import FAISS
+import tempfile
+
+query = """You will serve as an AI career coach, aiming to provide 
+insightful responses to the questions asked. Your objective 
+is to deliver helpful answers in character of AI career coach, 
+assisting users with their inquiries and refining their resume 
+details.
+
+You should maintain a friendly customer service tone.
+
+The resume information that users uploaded is listed between 
+the <data> XML like tags. You should reference when answering 
+the users questions {{user_input}}
+
+
+Here are some important rules for the interaction:
+
+Always stay in character, as AI career coach.
+If you are unsure how to repond, say 'Sorry, I didn't understand that. 
+Could you ask the question with more detailed information?''
+If someone asks something irrelevant, say,  'Sorry, I only give resume 
+related advice. Do you have a career question today I can help you with?'
+Ensure responses are within 50 words by default. If users desire more 
+information, increase the word count by 50 words with each request."""
 
 st.title('ResumeRev')
 st.subheader('The :blue[AI]-Powered Career Architect')
 
 col1, col2 = st.columns(2, gap="large")
+def initialize_chain(path_to_resume):
+    if "chain" in st.session_state:
+        del st.session_state["chain"]
 
+    # Load the PDF
+    loader = PyPDFLoader(path_to_resume)
 
-def upload_job_posting_and_resume_to_s3(job_posting, resume):
-    # Initialize a boto3 client
-    s3 = boto3.client('s3')
-    bucket_name = 'resume-team-2'
+    # Initialize Bedrock client
+    bedrock_runtime = boto3.client(
+        service_name="bedrock-runtime",
+        region_name="us-west-2",
+    )
 
-    # Define the S3 key names for the job posting and resume
-    session_id = str(uuid.uuid4())
+    embeddings, llm = BedrockEmbeddings(
+        model_id="amazon.titan-embed-text-v1",
+        client=bedrock_runtime,
+        region_name="us-west-2",
+    ), Bedrock(
+        model_id="anthropic.claude-v2", client=bedrock_runtime, region_name="us-west-2"
+    )
 
-    job_posting_key = f'{session_id}/job_posting.txt'
-    resume_key = f'{session_id}/{resume.name}'
+    # Create index from the PDF
+    index_creator = VectorstoreIndexCreator(
+        vectorstore_cls=FAISS,
+        embedding=embeddings,
+    )
 
-    try:
-        # Upload the job posting
-        # Convert the job posting string to bytes and upload it as a text file
-        s3.put_object(Body=job_posting.encode(), Bucket=bucket_name, Key=job_posting_key)
+    current_dir = os.getcwd()
 
-        # Upload the resume
-        # Use the 'UploadedFile' object's 'getbuffer()' method to read the file content
-        s3.upload_fileobj(resume, bucket_name, resume_key)
+    index_from_loader = index_creator.from_loaders([loader])
+    index_from_loader.vectorstore.save_local("/tmp")
+    faiss_index = FAISS.load_local("/tmp", embeddings)
 
-        print(f"Job posting and resume uploaded successfully to bucket '{bucket_name}'")
-    except NoCredentialsError:
-        print("Error: AWS credentials not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    retriever = faiss_index.as_retriever()
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=retriever
+    )
 
+    chain({
+        "question": query,
+        "chat_history": []
+    })
+
+    st.session_state['chain'] = chain
 
 with col1:
-    st.subheader('Job Posting', divider=True)
-    job_posting = st.text_area(
-        "Copy and paste a job posting here",
-        "",
-        )
-
     st.subheader('Resume', divider=True)
     resume = st.file_uploader("Choose a PDF file", type=['pdf'])
     if resume is not None:
         st.success(resume.name + ' Selected')
-        if st.button('Start Tailoring', type="primary"):
-            with st.spinner('Uploading...'):
-                upload_job_posting_and_resume_to_s3(job_posting, resume)
-
+        if st.button('Submit', type="primary"):
+            temp_dir = tempfile.mkdtemp()
+            path = os.path.join(temp_dir, resume.name)
+            with open(path, "wb") as f:
+                f.write(resume.getvalue())
+            initialize_chain(path)
 
 with col2:
-    st.subheader('Your tailored Resume', divider=True)
-    # with open("flower.png", "rb") as file:
-    #     btn = st.download_button(
-    #         label="Download image",
-    #         data=file,
-    #         file_name="flower.png",
-    #         mime="image/png"
-    #     )
-    text_contents = '''This is some text'''
-    st.download_button('Download Resume', text_contents)
-
-    # Chatbot
     st.subheader("AI Consultant", divider=True)
-    # Initialize chat history
+
+    # Initialize messages list in session state if not present
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
 
-    # Accept user input
-    if prompt := st.chat_input("What is up?"):
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
+    # Accept new user input and process it
+    if 'chain' in st.session_state:
+        prompt = st.chat_input("Ask me something about the resume")
+        if prompt is not None:
+            # Append user question to messages
+            st.session_state.messages.append({"role": "user", "content": prompt})
+
+            # Display user question
+            # with st.chat_message("user"):
+            #     st.markdown(prompt)
+
+            # Generate AI response using 'chain'
+            result = st.session_state['chain']({
+                "question": prompt,
+                "chat_history": st.session_state.chat_history
+            })
+            ai_response = result["answer"]
+
+            # Display AI response and append it to messages
+            # with st.chat_message("assistant"):
+            #     st.markdown(ai_response)
+
+            st.session_state.messages.append({"role": "assistant", "content": ai_response})
+            st.session_state.chat_history.append((prompt, ai_response))
+    else:
+        st.write("Please upload a resume to start the consultation.")
+
+    with st.container(height=400):
+        # Display existing chat messages
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
